111cd9648c8abd7bca7022cca95e6e2b90cc2632
william wen <wilhelm6939@gmail.com>
2019-03-08 00:07:37 +0800

    better

--- pwretry.py
+++ pwretry.py
@@ -1,13 +1,11 @@
 x = 3
-while x != 0:
+while x > 0:
 	pw = input ('請輸入密碼: ')
 	if pw == 'a123456':
 		print('登入成功')
 		break 
 	else:
-		x = x -1
-		if x == 0:
-			break
+		x = x - 1
 		print('密碼錯誤! 還有', x, '次機會')
 
 
